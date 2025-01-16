98e8d4189c228ea2d8147a4820a9042591f1c7c9
aditamairham66 <aditamairham66@gmail.com>
2025-01-16 14:25:38 +0700

    (feat): add benchmark unit test

--- helpers/hello_world_test.go
+++ helpers/hello_world_test.go
@@ -101,3 +101,60 @@ func TestHelloWorldKurniawan(t *testing.T) {
 
 	fmt.Println("TestHelloWorldKurniawan Done")
 }
+
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Eko")
+	}
+}
+
+func BenchmarkHelloWorldKurniawan(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Kurniawan")
+	}
+}
+
+func BenchmarkSub(b *testing.B) {
+	b.Run("Eko", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Eko")
+		}
+	})
+	b.Run("Kurniawan", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Kurniawan")
+		}
+	})
+}
+
+func BenchmarkTable(b *testing.B) {
+	benchmarks := []struct {
+		name    string
+		request string
+	}{
+		{
+			name:    "Eko",
+			request: "Eko",
+		},
+		{
+			name:    "Kurniawan",
+			request: "Kurniawan",
+		},
+		{
+			name:    "EkoKurniawanKhannedy",
+			request: "Eko Kurniawan Khannedy",
+		},
+		{
+			name:    "Budi",
+			request: "Budi Nugraha",
+		},
+	}
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HelloWorld(benchmark.request)
+			}
+		})
+	}
+}
